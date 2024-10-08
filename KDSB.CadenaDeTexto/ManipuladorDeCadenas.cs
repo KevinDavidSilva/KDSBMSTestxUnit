fbd004b2ce367849f91495360af11d4cb28fddd5
KevinSilva <elmersilva462@gmail.com>
2024-10-07 18:28:39 -0600

    clase ManipuladorDeCadenas

---
+++ KDSB.CadenaDeTexto/ManipuladorDeCadenas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDSB.CadenaDeTexto
+{
+    public class ManipuladorDeCadenas
+    {
+        public string ReverseString(string input)
+        {
+            char[] arrayCaracteres = input.ToCharArray();
+            Array.Reverse(arrayCaracteres);
+            return new string(arrayCaracteres);
+        }
+
+        public string RemoveSpaces(string input)
+        {
+            return input.Replace(" ", "");
+        }
+    }
+}
