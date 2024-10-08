4ca7e629bbda6951d80e0b8bea44e501f38a0e60
KevinSilva <elmersilva462@gmail.com>
2024-10-07 18:34:30 -0600

    xUnitTestx cadena de texto

---
+++ KDSB.Cadena.UnitTestxUnitTest/CadenaDeTextoTest.cs
@@ -0,0 +1,45 @@
+﻿using KDSB.CadenaDeTexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDSB.Cadena.UnitTestxUnitTest
+{
+    public class CadenaDeTextoTest
+    {
+        private readonly ManipuladorDeCadenas _manipuladorDeCadenas;
+
+        public CadenaDeTextoTest()
+        {
+            _manipuladorDeCadenas = new ManipuladorDeCadenas();
+        }
+
+        [Fact]
+        public void ReverseString_DeberiaInvertirCadena()
+        {
+            // Arrange
+            string entrada = "hola";
+
+            // Act
+            string resultado = _manipuladorDeCadenas.ReverseString(entrada);
+
+            // Assert
+            Assert.Equal("aloh", resultado);
+        }
+
+        [Fact]
+        public void RemoveSpaces_DeberiaEliminarEspaciosEnBlanco()
+        {
+            // Arrange
+            string entrada = "h o l a";
+
+            // Act
+            string resultado = _manipuladorDeCadenas.RemoveSpaces(entrada);
+
+            // Assert
+            Assert.Equal("hola", resultado);
+        }
+    }
+}
