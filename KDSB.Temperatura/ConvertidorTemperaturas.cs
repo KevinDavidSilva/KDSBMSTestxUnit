2c4a988f9b9c701ea92c8ac6af0f8e959227951b
KevinSilva <elmersilva462@gmail.com>
2024-10-07 18:20:48 -0600

    Clase para convertir temperatura

---
+++ KDSB.Temperatura/ConvertidorTemperaturas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDSB.Temperatura
+{
+    public class ConvertidorTemperaturas
+    {
+        public double CelsiusAFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public double FahrenheitACelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+    }
+}
